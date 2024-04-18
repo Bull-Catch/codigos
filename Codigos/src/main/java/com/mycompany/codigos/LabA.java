0f2e198f23261d621901f9123ab5d80afccbdcfc
admin <admin@gmail.com>
2024-04-18 13:46:01 -0500

    ejercicio 1.1

--- Codigos/src/main/java/com/mycompany/codigos/LabA.java
+++ Codigos/src/main/java/com/mycompany/codigos/LabA.java
@@ -8,16 +8,16 @@ public class LabA {
         Scanner lector = new Scanner(System.in);
         int n = lector.nextInt();
         int total = 0;
-        for (int i= 0; i<n; i=i+1){
-            int n1= lector.nextInt();
-            int n2= lector.nextInt();
-            int n3= lector.nextInt();
-            
-            if(n1+n2+n3>=2){
-                total= total+1;
+        for (int i = 0; i < n; i = i + 1) {
+            int n1 = lector.nextInt();
+            int n2 = lector.nextInt();
+            int n3 = lector.nextInt();
+
+            if (n1 + n2 + n3 >= 2) {
+                total = total + 1;
             }
         }
-            
-        System.out.println(total);         
+
+        System.out.println(total);
     }
 }
